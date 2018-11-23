a6d43a913be949b6dd9a09b06326545e3709ad4c
matbc92 <matheusbc92@gmail.com>
2018-11-23 19:14:25 +0000

    comitando hoje de novo

--- operadores.py
+++ operadores.py
@@ -1,11 +1,9 @@
-#+ adiciona dois itens(opratarios, ou operand)
-print(3+5)
+# + adiciona dois itens(opratarios, ou operand)
+print(3 + 5)
 print('a' + 'b')
-#- subtrai dois itens, se o primeiro item estiver omisso, ele é assumido zero
-#* multiplica dois numeros, ou repete uma string x vezes
-print('la'*3)
-#** executa uma potenciação
-print(3**4)
-#/ divide
-
-
+# - subtrai dois itens, se o primeiro item estiver omisso, ele é assumido zero
+# * multiplica dois numeros, ou repete uma string x vezes
+print('la' * 3)
+# ** executa uma potenciação
+print(3 ** 4)
+# / divide
