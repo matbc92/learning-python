77f167edea1bf2029acf1c6c20f3e62c119b06e6
matbc92 <matheusbc92@gmail.com>
2018-12-08 19:35:25 -0200

    terminada a lição de if

---
+++ expression.py
@@ -0,0 +1,9 @@
+length = 5
+breadth = 2
+
+area = length * breadth
+print('Area is', area)
+print('Perimeter is', 2 * (length + breadth))
+# isso é uma conta simples utilizando duas variaveis
+# uma reprsentando comprimento outra largura de alum objeto
+# resultando assim, na area de tal objeto(presumidadmente retangular
---
+++ if_notas.py
@@ -0,0 +1,17 @@
+#if condiciona o programa a executar um bloco de instruções se certas condições forem atendidas e outro(s) se condições diferentes ocorrerem
+numero = 30
+chute = int(input('Chute um numero: '))
+dois = 2
+
+if chute == 1:
+    print('Um? Não acha meio obvio? Tente um número um pouco maior')
+elif chute == dois:
+    print('Dois? mas dois é tão obvio, tente um número maior')
+elif chute == 3:
+    print('três, mas que numero mais bobo tente um pouquinho maior')
+elif chute < numero:
+    print('hmmm... não é esse, tente um numero maior')
+elif chute == numero:
+    print('parabens voce acertou, aqui está seu vale biscoito imaginário')
+else:
+    print('voce passou, tente um numero um pouco menor')
