02a9feace9237137c505ae2c66a79c91b0943bf1
matbc92 <matheusbc92@gmail.com>
2018-12-08 19:38:35 -0200

    pequeninas correçoe

--- if_notas.py
+++ if_notas.py
@@ -1,11 +1,10 @@
 #if condiciona o programa a executar um bloco de instruções se certas condições forem atendidas e outro(s) se condições diferentes ocorrerem
 numero = 30
 chute = int(input('Chute um numero: '))
-dois = 2
 
 if chute == 1:
     print('Um? Não acha meio obvio? Tente um número um pouco maior')
-elif chute == dois:
+elif chute == 2:
     print('Dois? mas dois é tão obvio, tente um número maior')
 elif chute == 3:
     print('três, mas que numero mais bobo tente um pouquinho maior')
