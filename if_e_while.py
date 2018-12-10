8ac59ccd150a91d73f57317002402a1146508132
matbc92 <matheusbc92@gmail.com>
2018-12-09 23:02:35 -0200

    aprendi o while, além do if

---
+++ if_e_while.py
@@ -0,0 +1,21 @@
+#if condiciona o programa a executar um bloco de instruções se certas condições forem atendidas e outro(s) se condições diferentes ocorrerem
+numero = 30
+running = True
+while running:
+    chute = int(input('Chute um numero: '))
+
+    if chute == 1:
+        print('Um? Não acha meio obvio? Tente um número um pouco maior')
+    elif chute == 2:
+        print('Dois? mas dois é tão obvio, tente um número maior')
+    elif chute == 3:
+        print('três, mas que numero mais bobo tente um pouquinho maior')
+    elif chute < numero:
+        print('hmmm... não é esse, tente um numero maior')
+    elif chute == numero:
+        print('parabens voce acertou, aqui está seu vale biscoito imaginário')
+        running = False
+    else:
+        print('voce passou, tente um numero um pouco menor')
+else:
+    print('the while loop is over')
