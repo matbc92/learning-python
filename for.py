2349481641e13eacd81352988d2b43083b7da8cc
matbc92 <matheusbc92@gmail.com>
2018-12-10 21:41:32 -0200

    começando a aprender o for

---
+++ for.py
@@ -0,0 +1,6 @@
+for i in range(1, 6):
+    print(i)
+else:
+    print('The for loop is over')
+rrange = list(range(1, 6))
+print(rrange)
--- if_e_while.py
+++ if_e_while.py
@@ -1,8 +1,17 @@
-#if condiciona o programa a executar um bloco de instruções se certas condições forem atendidas e outro(s) se condições diferentes ocorrerem
+#if condiciona o programa a executar um bloco de instruções se certas condições forem atendidas
+#  e outro(s) se condições diferentes ocorrerem, as condições diferentes podem ser especificadas
+# por meio do comando elif, que efetivamente cria um outro nó de if, ou simplesmente permanecerem
+# como qualquer coisa diferente da condição determinada por if
+########################################################################
+#já o comando while, separa um bloco de texto que será repetido enquanto o parametro determinado,
+# seja verdadeiro, ou que um comando de break aconteça, uma vez que o parametro se torna falso,
+# o programa retorna ao bloco de texto no qual o while está inserido(isto também é valido para o if),
+# a não ser que o comando else seja utilizado, sendo assim as instruções dentro deste comando seram executadas
+# antes do programa retornar ao bloco de codigo anterior.
 numero = 30
 running = True
 while running:
-    chute = int(input('Chute um numero: '))
+    chute = int(input('Tente adivinhar o número secreto: '))
 
     if chute == 1:
         print('Um? Não acha meio obvio? Tente um número um pouco maior')
@@ -12,10 +21,10 @@ while running:
         print('três, mas que numero mais bobo tente um pouquinho maior')
     elif chute < numero:
         print('hmmm... não é esse, tente um numero maior')
-    elif chute == numero:
+    elif chute > numero:
+        print('voce passou, tente um numero um pouco menor')
+    else:
         print('parabens voce acertou, aqui está seu vale biscoito imaginário')
         running = False
-    else:
-        print('voce passou, tente um numero um pouco menor')
 else:
-    print('the while loop is over')
+    print('o jogo terminou')
