9fc23e4845a628b574529099bd997737c0f854c4
Matbc92 <matheusbc92@gmail.com>
2019-01-16 21:55:31 -0200

    programinha de commit devidamente documentado

--- commitpush.py
+++ commitpush.py
@@ -1,10 +1,14 @@
+#programinha pra facilitar meus commmits
 import os
 if os.system('git add .') == 0:
     print('files added')
 else:
     print('failed to add files')
 messange = input()
-messange.replace(' ', '\ ')
+# por alguma razão os caracteres de espaço
+# resultantes da função input causam problemas
+# quando importados para o shell, portanto precisei
+# substituir eles por '\ '
 if os.system('git commit --m {}'.format(messange.replace(' ','\ '))) == 0:
     print('files commited')
 else:
