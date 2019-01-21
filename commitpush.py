d990471f5536c7eb32b9be2db17db01a2c33bf99
Matbc92 <matheusbc92@gmail.com>
2019-01-21 19:39:15 -0200

    concertei o workflow do commitpush

--- commitpush.py
+++ commitpush.py
@@ -2,15 +2,15 @@
 import os
 if os.system('git add .') == 0:
     print('files added')
+    #as aspas duplas dentro das aspas simples
+    # sao para indicar para o os, que o input é uma string
+    messenge = input('Insira o comment:')
+    if os.system('git commit --m "{}"'.format(messenge)) == 0:
+        print('files commited')
+    else:
+        print('failed to commit files')
+    os.system('git push')
 else:
     print('failed to add files')
-messange = input('Insira o comment:')
-# por alguma razão os caracteres de espaço
-# resultantes da função input causam problemas
-# quando importados para o shell, portanto precisei
-# substituir eles por '\ '
-if os.system('git commit --m "{}"'.format(messange)) == 0:
-    print('files commited')
-else:
-    print('failed to commit files')
-os.system('git push')
+
+
