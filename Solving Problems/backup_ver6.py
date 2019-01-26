0c908fb88b7a8d3494eb20d96d8cba29ad5485b2
Matbc92 <matheusbc92@gmail.com>
2019-01-26 00:23:03 -0200

    terminando a parte programação orientada a objeto

---
+++ Solving Problems/backup_ver6.py
@@ -0,0 +1,49 @@
+import os
+import time
+import shutil
+# 1. The files and directories to be backed up are
+# specified in a list.
+# Example on Windows:
+# source = ['"C:\\My Documents"']
+# Example on Mac OS X and Linux:
+source = '/home/matbc/Desktop'
+# Notice we have to use double quotes inside a string
+# for names with spaces in it.  We could have also used
+# a raw string by writing [r'C:\My Documents'].
+
+# 2. The backup must be stored in a
+# main backup directory
+# Example on Windows:
+# target_dir = 'E:\\Backup'
+# Example on Mac OS X and Linux:
+target_dir = '/home/matbc/backup_do_desktop'
+# Remember to change this to which folder you will be using
+# Create target directory if it is not present
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)  # make directory
+
+# 3. The files are backed up into a zip file.
+# 4. The name of the zip archive is the current date and time# 4. The current day is the name of the subdirectory
+# in the main directory.
+today = target_dir + os.sep + time.strftime('%d-%m-%Y')
+# The current time is the name of the zip archive.
+now = time.strftime('%H:%M:%S')
+
+# Take a comment from the user to
+# create the name of the zip file
+comment = input('Enter a comment --> ')
+# Check if a comment was entered
+if len(comment) == 0:
+    target = today + os.sep + now
+else:
+    target = today + os.sep + now + '_' +\
+             comment
+
+
+# Create the subdirectory if it isn't already there
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully created directory', today)
+
+shutil.make_archive(target,'zip', source)
+
---
+++ object oriented programming/oop_subclass.py
@@ -0,0 +1,59 @@
+# é possivel criar sublasses de classes ja existentes por meio do principio de herança, que eh utilizado pelo comando: class: subclasse(superclasse), o initi de uma super classe não é automaticamente ativado na criação de uma instancia de uma subclasse com init proprio(na ausencia de um init a subclasse herda o o init da superclasse), portanto quando isto é necessario ele deve ser chamado manualmente. Outra proprieade da herança é que uma classe pode herdar multiplas classes em paralelo, e isto afeta a propriedade de acesso aos metodos, que é dada da seguinte forma, quando um metodo é chamado para uma classe
+class SchoolMember:
+    '''Represents any school member.'''
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print('(Initialized SchoolMember: {})'.format(self.name))
+        self.member_phrase = 'Name:"{}" Age:"{}"'.format(name, age)
+
+    def tell(self):
+        '''Tell my details.'''
+        print (self.member_phrase)
+
+class Teacher(SchoolMember):
+    '''Represents a teacher.'''
+    def __init__(self, name, age, salary):
+        SchoolMember.__init__(self, name, age)
+        self.salary = salary
+        print ('(Initialized Teacher: {})'.format(self.name))
+        self.teacher_subphrase = 'Salary: "{:d}"'.format(self.salary)
+        self.teacher_phrase = str(self.member_phrase) + str(self.teacher_subphrase)
+    def tell(self):
+        SchoolMember.tell(self)
+        print (self.teacher_phrase)
+class Student(SchoolMember):
+    '''Represents a student.'''
+    def __init__(self, name, age, marks):
+        SchoolMember.__init__(self, name, age)
+        self.marks = marks
+        print ('(Initialized Student: {})'.format(self.name))
+        self.studen_subphrase = 'Marks: "{:d}"'.format(self.marks)
+        self.studen_phrase = self.member_phrase + self.studen_subphrase
+    def tell(self):
+        SchoolMember.tell(self)
+        print (self.studen_phrase)
+class Weirdo(Teacher, Student):
+    def __init__(self,name, age, marks, salary):
+        Teacher.__init__(self,name,age,salary)
+        Student.__init__(self,name,age,marks)
+        self.name = name
+        self.age = age
+        self.marks = marks
+        self.salary = salary
+        self.weirdo_phrase = self.member_phrase +self.studen_subphrase +self.teacher_subphrase
+        print('(Initialized Weirdo: {})'.format(self.name))
+    def tell(self):
+        print(self.weirdo_phrase)
+
+w = Weirdo('Harry', 30, 85, 20000)
+t = Teacher('Mrs. Shrividya', 40, 30000)
+s = Student('Swaroop', 25, 75)
+# prints a blank line
+print()
+members = [t, s, w]
+for member in members:
+# Works for both Teachers and Students
+    member.tell()
+
