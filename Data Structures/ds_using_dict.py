05bce72fa8dca76b1b7da4e28ff11613cfae2a78
matbc92 <matheusbc92@gmail.com>
2019-03-12 01:31:34 -0300

    começando um arquivo novo sobre definição de classes

--- Data Structures/ds_using_dict.py
+++ Data Structures/ds_using_dict.py
@@ -1,7 +1,7 @@
 #dict é uma estrutura definida por um par de colchetes no ao redor de seu conteudo,
-# tal estrutura permite associar keys e values(que devem ser definidos seguindo a estrutura key1: value1, key2: vallue2)
+# tal estrutura permite associar keys e values(que devem ser definidos seguindo a estrutura key1: value1, key2: value2)
 # sendo que keys sao efetivamentes as entradas do dicinoario(uma vez que dict eh abraviação de dictionary)
-# e value é o corpo desta entrada, na estrutura dict keys tem que ser sempre valores imutaveis como strings(aka objetos simples)
+# e value é o corpo desta entrada, na estrutura dict, keys tem que ser sempre valores imutaveis como strings(aka objetos simples)
 # tal restrição nao se aplica a values, todas as keys devem ser unicas.
 # itens adicionais podem ser incluidos num dicionario por meio da estrutura nome_do_dicionario[key] = value
 
